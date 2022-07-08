362602a908c4efc7b206a00cd4dc40bc2e59651e
Bruno Veiga <eubrunoveiga@gmail.com>
2022-07-08 13:48:31 -0300

    tentei mas não deu

--- src/fetchCharacter.js
+++ src/fetchCharacter.js
@@ -4,12 +4,14 @@ const fetchCharacter = async (id) => {
     const URL = `https://www.superheroapi.com/api.php/4192484924171229/${id}`;
     // Aguardamos a promise do fetch ser resolvida
     const promiseFetch = await fetch(URL)
+    console.log(promiseFetch);
     // Aguardamos a promise do json ser resolvida
     const results = await promiseFetch.json();
-
+    console.log(results);
     //Retornamos o resultado da nossa requisição
     return results;
   } catch (error) {
+    console.log(error);
     // Caso algo aconteça e nossa requisição não seja realizada com sucesso
     // capturamos e retornamos essa exceção.
     return error;
